ae57aa33cfb317c32b91599a2fd358be8ea456d4
afrid2004 <mdfaisalafrid@gmail.com>
2026-03-09 00:23:45 +0600

    added commit

--- src/components/Helmet/Title.jsx
+++ src/components/Helmet/Title.jsx
@@ -5,6 +5,7 @@ const Title = ({ title }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <link rel="icon" type="image/svg+xml" href="/images/favicon.png" />
     </Helmet>
   );
 };
